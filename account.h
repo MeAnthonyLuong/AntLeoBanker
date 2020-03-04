350c3d815db130cd3c00a16a6b85d9756ae9b14c
Anthony Luong <a.luong1997@edmail.edcc.edu>
2020-03-03 16:36:10 -0800

    account.h header added

--- account.h
+++ account.h
@@ -1,10 +1,40 @@
 //
-// Created by <Name> on <Date>.
+// Anthony Luong and Leo Mota-Villaraldo on 03/03/2020
 //
 
 #pragma once
+#include <queue>
+using namespace std;
+
+const int ACCOUNT_TYPES = 10;
+const string FUNDS[10] = {
+  	"Money Market",
+		"Prime Money Market",
+		"Long-Term Bond",
+		"Short-Term Bond",
+		"500 Index Fund",
+		"Capital Value Fund",
+		"Growth Equity Fund",
+		"Growth Index Fund",
+		"Value Fund",
+		"Value Stock Index"
+};
 
 class Account {
+  
+  friend ostream& operator<<(ostream& os, const Account& acc);
   public:
+    bool withdraw(int fundType, int amt);
+    bool deposit(int fundType, int amt);
+    
+    const queue<string> getHistory();
+
   private:
+    queue<string> history;
+    const static int MAX_ACC = 10;
+    int accNum;
+
+    int funds[MAX_ACC];
+    string firstName;
+    string lastName;
 };
