3b7745f5c8f7d7febb02b125a4753be21377cd5f
Anthony Luong <a.luong1997@edmail.edcc.edu>
2020-03-03 16:42:32 -0800

    bank.h made header

--- bank.h
+++ bank.h
@@ -1,5 +1,5 @@
 //
-// Created by <Name> on <Date>.
+// Anthony Luong and Leo Mota-Villaraldo on 03/03/2020
 //
 
 #pragma once
@@ -7,17 +7,30 @@
 #include "accounttree.h"
 #include <fstream>
 #include <string>
+#include <queue>
 
 using namespace std;
 
 class Bank {
+  friend ostream& operator<<(ostream& os, Bank& bank);
   public:
-    Bank();
+    Bank(string inputFile);
     ~Bank();
 
+    bool parseString(string line) const;
+    
     void processTransactions(const string& fileName);
     void displayAllBankBalances() const;
 
+    bool openAccount(string firstName, string lastName, int accNum);
+    bool withdrawAssets(int accNum, int fund, int amt);
+    bool transferAssets(int accNum1, int transferAmount, int accNum2);
+    bool depositAssets(int accNum, int amt, int fund);
+    void historyTransaction(int accNum);
   private:
+    bool insertAccounts(Account acc);
+    bool accountExists(Account acc);
+    AccountTree accounts;
+    queue<string> transaction;
     AccountTree accounts;
 };
