be040b07b55af6dc372076ab6a35997d68af8c0c
Leonardo Mota <leoism17@gmail.com>
2020-03-05 15:14:15 -0800

    Unfriended Account from Node The Account class was previously a friend of the Node class. Now, the Node class has getters and setters to avoid having to have account class as a friend.

--- accounttree.cpp
+++ accounttree.cpp
@@ -12,90 +12,97 @@ AccountTree::AccountTree() { root = nullptr; }
 AccountTree::~AccountTree() { clear(); }
 
 // Insert new account
-bool AccountTree::insert(Account *acc) {
-  if (this->root == nullptr) {
-    this->root = new Node(acc);
-    return true;
-  }
-  Node *curr = this->root;
-
-  while (curr != nullptr) {
-    if (acc->accNum > curr->account->accNum) {
-      if (curr->right == nullptr) {
-        curr->right = new Node(acc);
+bool AccountTree::insert(Account* acc) {
+    if (this->root == nullptr) {
+        this->root = new Node(acc);
         return true;
-      }
-      curr = curr->right;
-    } else if (acc->accNum < curr->account->accNum) {
-      if (curr->left == nullptr) {
-        curr->left = new Node(acc);
-        return true;
-      }
-      curr = curr->left;
     }
-  }
-  return false;
+    Node* curr = this->root;
+
+    while (curr != nullptr) {
+        if (acc->accNum > curr->getAccount()->accNum) {
+            if (curr->getRight() == nullptr) {
+                curr->setRight(new Node(acc));
+                return true;
+            }
+            curr = curr->getRight();
+        } else if (acc->accNum < curr->getAccount()->accNum) {
+            if (curr->getLeft() == nullptr) {
+                curr->setLeft(new Node(acc));
+                return true;
+            }
+            curr = curr->getLeft();
+        }
+    }
+    return false;
 }
 
 // Retrieve account
 // returns true if successful AND *account points to account
-bool AccountTree::retrieve(const int &accountNumber, Account *&account) const {
-  return true;
+bool AccountTree::retrieve(const int& accountNumber, Account*& account) const {
+    return true;
 }
 
-Account *AccountTree::getAccount(const int &accNum) {
-  getAccount(accNum, root);
+Account* AccountTree::getAccount(const int& accNum) {
+    getAccount(accNum, root);
 }
 
-Account *AccountTree::getAccount(const int &accNum, Node *curr) {
-  if (curr == nullptr) {
-    return nullptr;
-  }
+Account* AccountTree::getAccount(const int& accNum, Node* curr) {
+    if (curr == nullptr) {
+        return nullptr;
+    }
 
-  if (accNum == curr->account->accNum) {
-    return curr->account;
-  }
+    if (accNum == curr->getAccount()->accNum) {
+        return curr->getAccount();
+    }
 
-  if (accNum < curr->account->accNum) {
-    return getAccount(accNum, curr->left);
-  } else {
-    return getAccount(accNum, curr->right);
-  }
+    if (accNum < curr->getAccount()->accNum) {
+        return getAccount(accNum, curr->getLeft());
+    } else {
+        return getAccount(accNum, curr->getRight());
+    }
 }
+AccountTree::Node* AccountTree::Node::getRight() const { return right; }
+
+void AccountTree::Node::setRight(Node* newNode) { right = newNode; }
+
+Account* AccountTree::Node::getAccount() const { return account; }
+
+AccountTree::Node* AccountTree::Node::getLeft() const { return left; }
+
+void AccountTree::Node::setLeft(Node* newNode) { left = newNode; }
 
 // Display information on all accounts
 void AccountTree::display() const { display(root); }
 
-void AccountTree::display(Node *curr) const {
-  if (curr == nullptr) {
-    return;
-  }
-  display(curr->left);
-  cout << *(curr->account) << endl;
-  display(curr->right);
+void AccountTree::display(Node* curr) const {
+    if (curr == nullptr) {
+        return;
+    }
+    display(curr->getLeft());
+    cout << *(curr->getAccount()) << endl;
+    display(curr->getLeft());
 }
 
 // delete all information in AccountTree
 void AccountTree::clear() {
-  if (isEmpty()) {
-    cout << "There are no Accounts to delete" << endl;
-    return;
-  }
-  recursiveRemove(root);
-  if (root->right == nullptr && root->left == nullptr) {
-    delete root;
-  }
+    if (isEmpty()) {
+        cout << "There are no Accounts to delete" << endl;
+        return;
+    }
+    recursiveRemove(root);
 }
 
 // check if tree is empty
 bool AccountTree::isEmpty() const { return root == nullptr; }
 
-void AccountTree::recursiveRemove(Node *curr) {
-  if (isEmpty()) {
-    return;
-  }
-  recursiveRemove(curr->left);
-  recursiveRemove(curr->right);
-  delete curr->account;
-  delete curr;
+void AccountTree::recursiveRemove(Node* curr) {
+    if (curr == nullptr) {
+        return;
+    }
+    recursiveRemove(curr->getLeft());
+    recursiveRemove(curr->getRight());
+    Account* tmp = curr->getAccount();
+    delete tmp;
+    delete curr;
 }
